90d5c37f7d6ca83e78cc84d4e44dbba329c2fbb4
Matthew Sauve <samatthe@umich.edu>
2016-02-04 12:51:12 -0500

    added qsort - Q4

--- sort.c
+++ sort.c
@@ -6,8 +6,8 @@
 #include "sort.h"
 
 __attribute__ ((weak))
-int compare(int a, int b) {
-	    return a - b;
+int compare(const void *a, const void *b) {
+	    return (*(int*)a - *(int*)b);
 }
 
 void bubble_sort(int *numbers, unsigned count) {
@@ -16,7 +16,7 @@ void bubble_sort(int *numbers, unsigned count) {
 
 	for(i = 0; i < count; i++) {
 		for(j = 0; j < count - 1; j++) {
-			if(compare(numbers[j+1], numbers[j]) < 0) {
+			if(compare(&numbers[j+1], &numbers[j]) < 0) {
 				temp = numbers[j+1];
 				numbers[j+1] = numbers[j];
 				numbers[j] = temp;
@@ -39,7 +39,7 @@ void insertion_sort(int *numbers, unsigned count) {
 				break;
 			}
 
-			if (compare(num, new[idx]) < 0) {
+			if (compare(&num, &new[idx]) < 0) {
 				unsigned i;
 				for (i=new_count;i>idx;i--) {
 					new[i] = new[i-1];
@@ -57,5 +57,8 @@ void insertion_sort(int *numbers, unsigned count) {
 	memcpy(numbers, new, count*sizeof(int));
 }
 
-sorting_fn sorting_fns[] = {bubble_sort, insertion_sort, NULL};
+void Qsort(int *numbers, unsigned count) {
+	qsort(numbers, count, sizeof(int), compare);
+}
 
+sorting_fn sorting_fns[] = {bubble_sort, insertion_sort, Qsort, NULL};
